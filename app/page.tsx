97513776d41ab230bbfc2a47c31d8cc16b6fcf6f
Gregory <tahir.gregory.dev@gmail.com>
2024-11-29 15:15:58 +0100

    vérification metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import { radjhani } from './fonts/fonts.js'
 import type { Metadata } from 'next'
-import NosPresations from './components/Nos-prestations/Page.tsx'
-import AvisClients from '@/app/components/Avis-clients/Page.tsx'
-import QuiSommeNous from './components/Qui-somme-nous/Page.tsx'
 import MainLayout from './pages/layout.tsx'
+import React from 'react'
 
 // Metadata
 export const metadata: Metadata = {
@@ -12,6 +10,16 @@ export const metadata: Metadata = {
     "Avec 15 ans d'expérience, Vincent Parrot a fondé un garage à Toulouse pour offrir un service de réparation automobile de confiance et des voitures d'occasion de qualité. Son équipe qualifiée s'engage à fournir un service personnalisé et transparent pour répondre aux besoins spécifiques de chaque client.",
   robots: 'index, follow, noindex, nofollow, noarchive',
 }
+
+const QuiSommeNous = React.lazy(
+  () => import('./components/Qui-somme-nous/Page.tsx')
+)
+const NosPrestations = React.lazy(
+  () => import('./components/Nos-prestations/Page.tsx')
+)
+const AvisClients = React.lazy(
+  () => import('@/app/components/Avis-clients/Page.tsx')
+)
 
 export default function Home() {
   return (
@@ -24,7 +32,7 @@ export default function Home() {
         <h2 className={`mx-auto my-20 ${radjhani.className}`}>
           Nos prestations
         </h2>
-        <NosPresations />
+        <NosPrestations />
       </section>
 
       {/* Avis client */}
